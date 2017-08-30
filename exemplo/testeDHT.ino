24d04962b7bbff85ae1f24418d09e83d9904f167
Gabriel Faraco <gabriel.faraco@hotmail.com>
2017-08-30 15:25:51 -0400

    Update testeDHT.ino

--- exemplo/testeDHT.ino
+++ exemplo/testeDHT.ino
@@ -4,11 +4,7 @@
 #define DHTPIN A1 // pino que estamos conectado
 #define DHTTYPE DHT11 // DHT 11
  
-// Conecte pino 1 do sensor (esquerda) ao +5V
-// Conecte pino 2 do sensor ao pino de dados definido em seu Arduino
-// Conecte pino 4 do sensor ao GND
-// Conecte o resistor de 10K entre pin 2 (dados) 
-// e ao pino 1 (VCC) do sensor
+
 DHT dht(DHTPIN, DHTTYPE);
  
 void setup() 
